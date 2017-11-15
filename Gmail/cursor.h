9075602483cc1260973bc9fe04182029a3554235
LaurentEN <33687774+LaurentEN@users.noreply.github.com>
2017-11-15 14:17:08 +0100

    Update cursor.h

--- Gmail/cursor.h
+++ Gmail/cursor.h
@@ -5,6 +5,7 @@ int UP_ARROW[3] = {27, 91, 65};
 int DOWN_ARROW[3] = {27, 91, 66};
 int RIGHT_ARROW[3] = {27, 91, 67};
 int LEFT_ARROW[3] = {27, 91, 68};
+int DELETE_KEY[4] = {27, 91, 51, 126};
 
 struct cursor;
 
