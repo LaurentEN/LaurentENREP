f9c564567ba889e51ab8d98e1460785072486089
LaurentEN <33687774+LaurentEN@users.noreply.github.com>
2017-11-16 21:17:53 +0100

    Update checkSum.cpp

--- Gmail/checkSum.cpp
+++ Gmail/checkSum.cpp
@@ -1,1 +1,14 @@
 #include "checkSum.h"
+
+int checkSum(int *a, int *b) {
+    if(sizeof(a) != sizeof(b))
+        return 1;
+    else if(sizeof(a) == sizeof(b)) {
+        int size = sizeof(a);
+        for(int i = 0; i < size; i++) {
+            if(a[i] != b[i])
+                return 1;
+        }
+        return SUMS_EQUAL;
+    }
+}
