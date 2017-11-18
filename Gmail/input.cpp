4e8739d186adc3a3a0acf307338b7fd08e0aa95a
LaurentEN <33687774+LaurentEN@users.noreply.github.com>
2017-11-18 14:25:53 +0100

    Update input.cpp

--- Gmail/input.cpp
+++ Gmail/input.cpp
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <ncurses.h>
+#include "input.h"
+#include "cursor.h"
 
 void inputHandler() {
-    
+        initscr();
+        int *input;
+        int i(0);
+        while(input[0] != CTRL_X) {
+                intput[i] = getch();
+                if(input[i] <= 32 && input[i] >= 126) {
+                        placeCursorInStruct(input);
+                        input[i] = 0;
+                }
+                else if(input[i] == 27) {
+                        while(input[i++] = getch() && i != 2);
+                        placeCursorInStruct(input);
+                }
+                else {
+                        while(input[i++] = getch() && i != 3);
+                        placeCursorInStruct(input);
+                }
+        }
+        endwin();
 }
